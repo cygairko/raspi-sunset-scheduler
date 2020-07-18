09fc97c8740e4896dfe42a8c42484aa7977f94be
cygairko <cygairko@gmail.com>
2020-07-18 23:26:41 +0200

    skyfield

--- calc.py
+++ calc.py
@@ -7,7 +7,7 @@ from math import degrees, radians, sin, cos, asin, acos
 from pytz import timezone
 import tzlocal
 import argparse
-import skyfield
+from skyfield import api
 
 EVENT_NOON = 'noon'
 EVENT_SUNSET = 'sunset'
